250b942c9a429a2a15636dcb8bb630e944a22f20
thehao <chungthehao@gmail.com>
2019-07-18 19:23:00 +0700

    Entity methods

--- src/Controller/BookmarksController.php
+++ src/Controller/BookmarksController.php
@@ -20,7 +20,7 @@ class BookmarksController extends AppController
     public function index()
     {
         $this->paginate = [
-            'contain' => ['Users']
+            'contain' => ['Users', 'Tags']
         ];
         $bookmarks = $this->paginate($this->Bookmarks);
 
